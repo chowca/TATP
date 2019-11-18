d1548beb471b80bb044a90c2aba1c0822d95679a
Calvin Chow <calvin.chow@oocl.com>
2019-11-18 16:05:59 +0800

    Variable, constant and methods Upload

---
+++ Pre-training/Week 4-5 Back-End Programme - Java/JavaWorkSpace/codeTesting/src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        int x = 1;
+        System.out.println(X);
+    }
+}
