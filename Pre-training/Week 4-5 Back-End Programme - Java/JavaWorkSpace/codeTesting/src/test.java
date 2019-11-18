90be5df771550a829f722028b29c618e719fcffb
Calvin Chow <calvin.chow@oocl.com>
2019-11-18 20:24:34 +0800

    Exception handling Upload

--- Pre-training/Week 4-5 Back-End Programme - Java/JavaWorkSpace/codeTesting/src/test.java
+++ Pre-training/Week 4-5 Back-End Programme - Java/JavaWorkSpace/codeTesting/src/test.java
@@ -1,10 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-        boolean a = true;
-        boolean b = false;
-        System.out.println(a ^ b);
-
-
+        String s = null; s.concat("abc");
     }
 }
